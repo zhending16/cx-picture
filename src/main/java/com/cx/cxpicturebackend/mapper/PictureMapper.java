84a1a384dce71182b125fab6b6a1cf786df69ac2
17827646956 <2087144438@qq.com>
2025-07-18 16:37:39 +0800

    用户模块改造-引入邮箱注册/登录

---
+++ src/main/java/com/cx/cxpicturebackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.cx.cxpicturebackend.mapper;
+
+import com.cx.cxpicturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86178
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-06-03 23:54:56
+* @Entity com.cx.cxpicturebackend.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
